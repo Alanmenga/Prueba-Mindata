1374633a30346e7c750b78f235ae30f6b0ac779a
Alanmenga <alan.gabriel.mengassini@gmail.com>
2025-02-24 20:31:44 -0300

    cambios en la card de ejemplo

--- src/app/components/menu-heroes/menu-heroes.component.ts
+++ src/app/components/menu-heroes/menu-heroes.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-menu-heroes',
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './menu-heroes.component.html',
   styleUrl: './menu-heroes.component.css'
 })
