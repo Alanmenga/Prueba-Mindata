1b3b533c44bdf9e3b380611d7702f46c87eaf528
Alanmenga <alan.gabriel.mengassini@gmail.com>
2025-02-26 12:56:39 -0300

    agrego ruteo y limpieza codigo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { MenuHeroesComponent } from './components/menu-heroes/menu-heroes.component';
-import { HeroeComponent } from './components/heroe/heroe.component';
+import { InfoComponent } from './components/info/info.component';
 
 export const routes: Routes = [
     { path: 'menu-heroes', component: MenuHeroesComponent},
-    { path: 'info', component:HeroeComponent },
+    { path: 'info', component:InfoComponent },
     { path: '', redirectTo: '/menu-heroes', pathMatch: 'full' } 
 ];
---
+++ src/app/components/info/info.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroe',
+  imports: [],
+  templateUrl: './info.component.html',
+  styleUrl: './info.component.css'
+})
+export class InfoComponent {
+
+}
